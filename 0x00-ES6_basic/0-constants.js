9c50e22ab74b6c30f0b65387b5636b1e114c1ab9
OtienoOdongo <otabiznake@gmail.com>
2023-09-26 09:18:21 +0000

    ES6 const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,16 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
-
-
 
 export function getLast() {
   return ' is okay';
 }
 
-
-
 export function taskNext() {
-	let combination = 'But sometimes let';
-	const last = getLast(); // Getting the last part separately
-	combination += last;
+  let combination = 'But sometimes let';
+  const last = getLast(); // Getting the last part separately
+  combination += last;
 
-	return combination;
+  return combination;
 }
