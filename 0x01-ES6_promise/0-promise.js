d8603027a377696b23cfcb2233b51668dde0e861
OtienoOdongo <otabiznake@gmail.com>
2023-09-27 12:06:57 +0000

    ES6 promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+  const myPromise = new Promise((resolve, reject) => {
+    resolve();
+    // eslint-disable-next-line no-unused-vars
+    reject();
+  });
+
+  return myPromise;
+}
