0c512f95a3db62e1e5a9bebfff59ef7939c7912d
OtienoOdongo <otabiznake@gmail.com>
2023-09-28 15:54:56 +0000

    intro to ES6 private classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // the underscore indicates that the property is private
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
