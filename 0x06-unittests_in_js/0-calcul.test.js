0719e5bcbaf9b22f7ac4168cac23da223bed8e8e
OtienoOdongo <otabiznake@gmail.com>
2023-11-23 12:14:41 -0800

    unittest in js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,46 +5,24 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  // Test case: it should round the first parameter and return the sum
-  it('should round the first parameter and return the sum', () => {
-    // Test when the first parameter is a whole number
-    assert.equal(calculateNumber(1, 3), 4);
-    // Test when the first parameter is a decimal number
-    assert.equal(calculateNumber(1.2, 3), 4);
-    // Test when the first parameter is zero
-    assert.equal(calculateNumber(0, 3), 3);
+  it('should round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
 
-  // Test case: it should round the second parameter and return the sum
-  it('should round the second parameter and return the sum', () => {
-    // Test when the second parameter is a whole number
-    assert.equal(calculateNumber(1, 3.7), 5);
-    // Test when the second parameter is a decimal number
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    // Test when the second parameter is zero
-    assert.equal(calculateNumber(1, 0), 1);
+  it('should round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  // Test case: it should handle both parameters as whole numbers
-  it('should handle both parameters as whole numbers', () => {
-    assert.equal(calculateNumber(2, 3), 5);
-    assert.equal(calculateNumber(10, 5), 15);
-  });
-
-  // Test case: it should handle both parameters as decimal numbers
-  it('should handle both parameters as decimal numbers', () => {
-    assert.equal(calculateNumber(2.5, 3.8), 7);
-    assert.equal(calculateNumber(1.1, 2.9), 4);
-  });
-
-  // Test case: it should handle one parameter as zero
-  it('should handle one parameter as zero', () => {
-    assert.equal(calculateNumber(0, 5), 5);
-    assert.equal(calculateNumber(3, 0), 3);
-  });
-
-  // Test case: it should handle both parameters as zero
-  it('should handle both parameters as zero', () => {
-    assert.equal(calculateNumber(0, 0), 0);
+  it('should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
