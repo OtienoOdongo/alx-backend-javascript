f6268e137885a49e91e422532bb159bfbf99ed24
OtienoOdongo <otabiznake@gmail.com>
2023-11-23 13:04:03 -0800

    unittest in js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,33 +2,82 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  // Test cases for SUM
-  describe('SUM', () => {
-    it('should round the numbers and return the sum', () => {
-      assert.equal(calculateNumber('SUM', 1.0, 0), 1);
-      assert.equal(calculateNumber('SUM', 1.3, 0), 1);
-      assert.equal(calculateNumber('SUM', 1.7, 0), 2);
+  describe('SUM without rounding', () => {
+    it('should return 5', () => {
+      assert.equal(calculateNumber('SUM', 1, 4), 5);
     });
   });
 
-  // Test cases for SUBTRACT
-  describe('SUBTRACT', () => {
-    it('should round the numbers and return the subtraction result', () => {
-      assert.equal(calculateNumber('SUBTRACT', 2.5, 1.2), 1);
-      assert.equal(calculateNumber('SUBTRACT', 5.7, 2.8), 3);
-      assert.equal(calculateNumber('SUBTRACT', 10.4, 3), 7);
+  describe('SUM with the first number rounded', () => {
+    it('should return 6', () => {
+      assert.equal(calculateNumber('SUM', 2.4, 4), 6);
     });
   });
 
-  // Test cases for DIVIDE
-  describe('DIVIDE', () => {
-    it('should round the numbers and return the division result', () => {
-      assert.equal(calculateNumber('DIVIDE', 8, 2), 4);
-      assert.equal(calculateNumber('DIVIDE', 10, 3), 3.33);
-    });
-
-    it('should return Error when dividing by zero', () => {
-      assert.equal(calculateNumber('DIVIDE', 6, 0), 'Error');
+  describe('SUM with the second number rounded', () => {
+    it('should return 6', () => {
+      assert.equal(calculateNumber('SUM', 4, 2.4), 6);
     });
   });
+
+  describe('SUM with both numbers rounded', () => {
+    it('should return 6', () => {
+      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT without rounding', () => {
+    it('should return 2', () => {
+      assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
+    });
+  });
+
+  describe('SUBTRACT with the first number rounded', () => {
+    it('should return -3', () => {
+      assert.equal(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    });
+  });
+
+  describe('SUBTRACT with the second number rounded', () => {
+    it('should return 3', () => {
+      assert.equal(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    });
+  });
+
+  describe('SUBTRACT with both numbers rounded', () => {
+    it('should return -4', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE without rounding', () => {
+    it('should return 2', () => {
+      assert.equal(calculateNumber('DIVIDE', 8, 4), 2);
+    });
+  });
+
+  describe('DIVIDE with the first number rounded', () => {
+    it('should return 5', () => {
+      assert.equal(calculateNumber('DIVIDE', 9.5, 2), 5);
+    });
+  });
+
+  describe('DIVIDE with the second number rounded', () => {
+    it('should return 0.2', () => {
+      assert.equal(calculateNumber('DIVIDE', 2, 9.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE with both numbers rounded', () => {
+    it('should return 0.2', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE with error (division by zero)', () => {
+    it('should return Error', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
 
