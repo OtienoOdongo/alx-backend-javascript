281288376acf97ceb699734c2c41a470438ab14e
OtienoOdongo <otabiznake@gmail.com>
2023-11-24 05:21:31 -0800

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,37 +4,22 @@ const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const handleRequest = (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    handleStudentsRequest(res);
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found\n');
-  }
-};
-
-const handleStudentsRequest = (res) => {
-  res.write('This is the list of our students\n');
-  students(process.argv[2])
-    .then((data) => {
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}
-        .List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}
-        .List: ${data.sweStudents.join(', ')}`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
       res.end();
-    })
-    .catch((err) => res.end(err.message));
-};
-
-const app = http.createServer(handleRequest);
+    }).catch((err) => res.end(err.message));
+  }
+});
 
 app.listen(port, hostname);
 
 module.exports = app;
-
