44342c16d5443441aff52be4e6cf030c778c1096
OtienoOdongo <otabiznake@gmail.com>
2023-11-24 05:13:23 -0800

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,36 @@
 const fs = require('fs').promises;
 
 const countStudents = async (path) => {
-  let data;
   try {
-    data = await fs.readFile(path, 'utf8');
+    const data = await fs.readFile(path, 'utf8');
+
+    const parseStudent = (student) => ({
+      firstName: student[0],
+      lastName: student[1],
+      age: student[2],
+      field: student[3],
+    });
+
+    const students = data.split('\n')
+      .map((student) => student.split(','))
+      .filter((student) => student.length === 4 && student[0] !== 'firstname')
+      .map(parseStudent);
+
+    const filterAndMapStudents = (students, field) => students
+      .filter((student) => student.field === field)
+      .map((student) => student.firstName);
+
+    const csStudents = filterAndMapStudents(students, 'CS');
+    const sweStudents = filterAndMapStudents(students, 'SWE');
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+
+    return { students, csStudents, sweStudents };
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-
-  const parseStudent = (student) => ({
-    firstName: student[0],
-    lastName: student[1],
-    age: student[2],
-    field: student[3],
-  });
-
-  const students = data.split('\n')
-    .map((student) => student.split(','))
-    .filter((student) => student.length === 4 && student[0] !== 'firstname')
-    .map(parseStudent);
-
-  const filterAndMapStudents = (students, field) =>
-    students.filter((student) => student.field === field)
-    .map((student) => student.firstName);
-
-  const csStudents = filterAndMapStudents(students, 'CS');
-  const sweStudents = filterAndMapStudents(students, 'SWE');
-
-  console.log(`Number of students: ${students.length}`);
-  console.log(`Number of students in CS: ${csStudents.length}
-  .List: ${csStudents.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweStudents.length}
-  .List: ${sweStudents.join(', ')}`);
-
-  return { students, csStudents, sweStudents };
 };
 
 module.exports = countStudents;
-
