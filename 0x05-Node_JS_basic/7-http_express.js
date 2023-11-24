d48abe3f08b3022ecea611f196c87fe59e8349f3
OtienoOdongo <otabiznake@gmail.com>
2023-11-24 05:49:10 -0800

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,50 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
 
 const app = express();
 const port = 1245;
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, records) => {
+    if (err) reject(new Error('Cannot load the database'));
+    else {
+      const lines = records.split('\n');
+      const csList = [];
+      const sweList = [];
+
+      lines.forEach((record) => {
+        const fields = record.split(',');
+        if (fields.length > 0 && fields !== null) {
+          if (fields[3] === 'CS') {
+            csList.push(fields[0]);
+          } else if (fields[3] === 'SWE') {
+            sweList.push(fields[0]);
+          }
+        }
+      });
+
+      let str = `Number of students: ${csList.length + sweList.length}\n`;
+      str += `Number of students in CS: ${csList.length}. List: ${csList.join(', ')}\n`;
+      str += `Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`;
+      resolve(str);
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  const databaseFileName = '/alx-backend-javascript/0x05-Node_JS_basic';
-
-  // Read the content of the CSV file asynchronously
-  fs.readFile(databaseFileName, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    // Split the content into lines
-    const lines = data.split('\n');
-
-    // Filter out empty lines
-    const nonEmptyLines = lines.filter((line) => line.trim() !== '');
-
-    // Display the non-empty lines in the response
-    const result = nonEmptyLines.join('\n');
-    res.send(`This is the list of our students:\n${result}`);
-  });
+app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
+  const database = process.argv.length > 2 ? process.argv[2] : '';
+  try {
+    const content = await countStudents(database);
+    res.write(content);
+  } catch (err) {
+    res.write(err.message);
+  }
+  res.end();
 });
 
 app.listen(port);
